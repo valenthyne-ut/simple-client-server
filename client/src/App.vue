6ea50831648d190ca0a8288a4c19af5df9c198f4
valenthyne-ut <valenthyne.ut@gmail.com>
2024-02-25 16:03:41 +0000

    Create simple Hello World client

---
+++ client/src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+	<main class="w-full h-screen flex justify-center items-center bg-zinc-800">
+		<h1 class="text-3xl text-white font-bold">Hello World!</h1>
+	</main>
+</template>
