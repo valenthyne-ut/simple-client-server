18f36f5c6518d78b7c043df2a1f3ff73d1d95879
valenthyne-ut <valenthyne.ut@gmail.com>
2024-02-28 20:51:18 +0000

    Server serves htdocs statically

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,9 +3,12 @@ import express from "express";
 import cors from "cors";
 import config from "./config";
 import { createServer } from "https";
+import { join } from "path";
 
 (async () => {
 	const app = express();
+
+	app.use(express.static(join(__dirname, "htdocs")));
 
 	app.use(cors({
 		origin: `https://localhost:${config.PORT}`
